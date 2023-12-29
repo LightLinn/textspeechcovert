3550157cc28ee3adddd92420764422e9b0695746
Light Lin <lightlin@LightdeacStudio.hitronhub.home>
2023-12-29 17:03:49 +0800

    v5

--- client/app/page.js
+++ client/app/page.js
@@ -2,17 +2,8 @@
 
 import Image from 'next/image'
 import Main from '@/components/main/Main'
-import { useRouter } from 'next/navigation';
-import { AuthContext } from '@/context/AuthContext';
 
 export default function Home() {
-  const router = useRouter();
-  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
-
-  if (!isLoggedIn){
-    router.push('/login')
-  }
-
   return (
     <div>
       <Main />
